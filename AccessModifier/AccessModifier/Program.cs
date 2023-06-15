3b13ab36b20c80aa2058ee804b930a7568da8069
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-06-15 10:11:57 +0530

    [Shivraj]Add:AccessModifier

---
+++ AccessModifier/AccessModifier/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifier
+{
+    public class Program : ProtectedAccessModifier
+    {
+        static void Main(string[] args)
+        {
+            PublicAccessModifier publicAccessModifier = new PublicAccessModifier();
+            publicAccessModifier.AccessModifier();
+            Console.ReadLine();
+
+            PrivateAccessModifier demo = new PrivateAccessModifier();
+            demo.Intro();
+            Console.ReadLine();
+
+            ProtectedAccessModifier protectedAccessModifier = new ProtectedAccessModifier();
+            Program program = new Program();
+            program.IntroOfProtectedModifier();
+            Console.ReadLine();
+
+            InternalAccessModifier internalAccessModifier = new InternalAccessModifier();
+            internalAccessModifier.IntroOfInternalmodifier();
+            Console.ReadLine();
+        }
+    }
+}
